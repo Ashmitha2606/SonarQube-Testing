05f4a867b73f5f8bf994673fc0c4d3293e034613
Ashmitha2606 <yenimireddyashmitha@gmail.com>
2026-04-02 15:11:16 +0530

    added bad code for sonar test

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function check(a,b){
-    if(a == b){
+function check(a, b) {
+    if (a == b) {
         console.log("Equal")
     }
-    eval("console.log('bad practice')")
+
+    eval("console.log('This is unsafe')")
+
+    var x = 10
+    var x = 20
+
+    return x
 }
 
-check(5,"5")
+check(5, "5")
